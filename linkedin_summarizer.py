3fbb319544e4e3aa05473da8e40f8de9ab118da9
gmachin-stratio <gmachin@stratio.com>
2024-10-07 11:04:10 +0200

    Update linkedin_summarizer.py

--- linkedin_summarizer.py
+++ linkedin_summarizer.py
@@ -4,8 +4,10 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
-from third_party.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_look_agent
+from third_party.linkedin import scrape_linkedin_profile
+
+from output_parsers import summary_parser
 
 
 # Code
@@ -15,7 +17,7 @@ def linkedin_summarizer(name: str) -> str:
     # Get LinkedIn Profile URL
     linkedin_username = linkedin_look_agent(name=name)
 
-    # Get information about LinkedIn Profile
+    # Get information about a LinkedIn Profile
     linkedin_data = scrape_linkedin_profile(
         linkedin_profile_url=linkedin_username,
         mock=True
@@ -26,17 +28,23 @@ def linkedin_summarizer(name: str) -> str:
         Given the following information {information} about a person, I want you to return:
         1. A brief summary
         2. Two interesting facts about the person
+        
+        The output must follow the following format {format_instructions}
         """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()}
     )
 
-    # LLM
+    # Instance LLM
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    # Chain
-    chain = summary_prompt_template | llm | StrOutputParser()
+    # Instance Chain
+    # chain = summary_prompt_template | llm
+    # chain = summary_prompt_template | llm | StrOutputParser()
+    chain = summary_prompt_template | llm | summary_parser
 
     # Invoke Chain
     res = chain.invoke(input={"information": linkedin_data})
